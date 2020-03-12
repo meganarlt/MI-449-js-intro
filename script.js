a28aa4d2bd85b381732fb9dea5c3b03600470486
Megan Arlt <meganarlt@Megans-MacBook-Pro-2.local>
2020-03-12 14:39:41 -0400

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+var x_1 = document.getElementById("myAudio_1"); 
+function playAudio_1() { 
+  x_1.play(); 
+} 
+
+var x_2 = document.getElementById("myAudio_2"); 
+function playAudio_2() { 
+  x_2.play(); 
+} 
+
+var x_3 = document.getElementById("myAudio_3"); 
+function playAudio_3() { 
+  x_3.play(); 
+} 
